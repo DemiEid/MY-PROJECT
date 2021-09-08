18adf63449293ba8b21152a99f63cb6603dbb0f4
DemiEid <69729787+DemiEid@users.noreply.github.com>
2021-09-08 19:21:33 +0100

    Add files via upload

---
+++ project/login.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="login.aspx.cs" Inherits="project.login" %>
+
+
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" href="bootstrap.css">
+
+<!------ Include the above in your HEAD tag ---------->
+
+ 
+
+<form id="form1" runat="server">
+
+ 
+
+<div class="wrapper fadeInDown">
+
+  <div id="formContent">
+
+    <!-- Tabs Titles -->
+
+ 
+
+    <!-- Icon -->
+
+    <div class="fadeIn first">
+
+      <img src="IMG_2100.png" id="icon" alt="User Icon" />
+
+    </div>
+
+ 
+
+    <!-- Login Form -->
+
+      <asp:TextBox runat="server" class="fadeIn second" id="usernameTxt" placeholder="id" OnTextChanged="usernameTxt_TextChanged"></asp:TextBox>
+
+      <asp:TextBox runat="server" class="fadeIn third password" id="passwordTxt" placeholder="password"></asp:TextBox>
+
+      <asp:Button ID="Button1" runat="server" Text="Log In" class="fadeIn fourth" OnClick="Button1_Click" />
+
+   
+
+    <!-- Remind Password -->
+
+    <div id="formFooter">
+
+      <a class="underlineHover" href="#">Forgot Password?</a>
+
+    </div>
+
+ 
+
+  </div>
+
+</div>
+
+</form>
+
+ 
---
+++ project/login.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+
+
+namespace project
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            using (SqlConnection connection = new SqlConnection("Server=localhost;Database=master;Trusted_Connection=True;"
+))
+            {
+                String id = usernameTxt.Text;
+                String password = passwordTxt.Text;
+
+                String storedPassword = "";
+                String getPasswordQuery = "SELECT password FROM dbo.[Table] WHERE Id = @Id;";
+                using (SqlCommand command = new SqlCommand(getPasswordQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+                    connection.Open();
+                    SqlDataReader dr = command.ExecuteReader();
+                    //get results of query into string
+                    while (dr.Read())
+                    {
+                        //string value = query result
+                        storedPassword = dr[0].ToString();
+                    }
+                    connection.Close();
+
+                    if (storedPassword == password)
+                    {
+                        usernameTxt.Text = "login successful";
+
+                    }
+                    else {
+                        usernameTxt.Text = "login unsuccessful";
+                    }
+                }
+
+            }
+
+
+
+
+
+        }
+
+        protected void usernameTxt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+    }
